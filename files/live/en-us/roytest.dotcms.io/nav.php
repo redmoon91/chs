56e1c7c1fcd0c7728a871f807da174e361136f25
Dean Gonzalez <dean@dotcms.com>
2024-10-09 14:55:37 -0400

    dotCLI commit

---
+++ files/live/en-us/roytest.dotcms.io/nav.php
@@ -0,0 +1,15 @@
+<ul class="primaryMenu desktopvp">
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+</ul>
+
+<ul class="primaryMenu mobilevp">
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+    <li><a href="">Nav Link</a></li>
+</ul>
